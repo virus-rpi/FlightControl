b6f2b65c5cffcdf54313018f523285da485a3531
virusrpi <uwu@owo.computer>
2024-09-05 05:49:26 +0200

    fix: reduce plane polygons for performance

--- ThreeDView/models.go
+++ ThreeDView/models.go
@@ -26,28 +26,30 @@ func NewCube(size float64, position Point3D, rotation Point3D, color color.Color
 	}
 }
 
-func NewPlane(size float64, position Point3D, rotation Point3D, color color.Color, w *ThreeDWidget) ThreeDShape {
-	gridSize := int(size / 10)
+func NewPlane(size float64, position Point3D, rotation Point3D, color color.Color, w *ThreeDWidget, resolution int) ThreeDShape {
 	half := size / 2
-	numVertices := (gridSize + 1) * (gridSize + 1)
-	vertices := make([]Point3D, numVertices)
-	faces := make([][3]int, gridSize*gridSize*2)
-
-	index := 0
-	for i := 0; i <= gridSize; i++ {
-		for j := 0; j <= gridSize; j++ {
-			x := -half + float64(i)*10
-			y := -half + float64(j)*10
-			vertices[index] = Point3D{X: x, Y: y, Z: 0}
-			index++
+	step := size / float64(resolution)
+	var vertices []Point3D
+	for i := 0; i <= resolution; i++ {
+		for j := 0; j <= resolution; j++ {
+			vertices = append(vertices, Point3D{
+				X: -half + float64(i)*step,
+				Y: -half + float64(j)*step,
+				Z: 0,
+			})
 		}
 	}
 
-	for i := 0; i < gridSize; i++ {
-		for j := 0; j < gridSize; j++ {
-			index = i*(gridSize+1) + j
-			faces[i*gridSize*2+j*2] = [3]int{index, index + 1, index + gridSize + 1}
-			faces[i*gridSize*2+j*2+1] = [3]int{index + 1, index + gridSize + 2, index + gridSize + 1}
+	var faces [][3]int
+	for i := 0; i < resolution; i++ {
+		for j := 0; j < resolution; j++ {
+			topLeft := i*(resolution+1) + j
+			topRight := topLeft + 1
+			bottomLeft := topLeft + (resolution + 1)
+			bottomRight := bottomLeft + 1
+
+			faces = append(faces, [3]int{topLeft, topRight, bottomRight})
+			faces = append(faces, [3]int{topLeft, bottomRight, bottomLeft})
 		}
 	}
 
--- simulation.go
+++ simulation.go
@@ -9,7 +9,7 @@ import (
 func simulationTab() fyne.CanvasObject {
 	threeDEnv := ThreeDView.NewThreeDWidget()
 	threeDEnv.SetBackgroundColor(color.RGBA{R: 135, G: 206, B: 235, A: 255})
-	plane := ThreeDView.NewPlane(1000, ThreeDView.Point3D{X: 0, Y: 0, Z: 0}, ThreeDView.Point3D{X: 0, Y: 0, Z: 0}, color.RGBA{G: 255, A: 255}, threeDEnv)
+	plane := ThreeDView.NewPlane(1000, ThreeDView.Point3D{X: 0, Y: 0, Z: 0}, ThreeDView.Point3D{X: 0, Y: 0, Z: 0}, color.RGBA{G: 255, A: 255}, threeDEnv, 10)
 	cube := ThreeDView.NewCube(100, ThreeDView.Point3D{X: 0, Y: 0, Z: 100}, ThreeDView.Point3D{X: 0, Y: 0, Z: 0}, color.RGBA{B: 255, A: 255}, threeDEnv)
 	camera := ThreeDView.NewCamera(ThreeDView.Point3D{Y: 500, Z: 200}, ThreeDView.Point3D{}, 30, 10)
 	threeDEnv.AddObject(&plane)
