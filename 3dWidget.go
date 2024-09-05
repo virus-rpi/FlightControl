6622668236a1a3eb9a34d3cb9988dc4bc9d333e9
virusrpi <uwu@owo.computer>
2024-09-05 03:09:19 +0200

    feat: faster and more accurate rendering

--- 3dWidget.go
+++ 3dWidget.go
@@ -8,12 +8,14 @@ import (
 	"image/color"
 	"image/draw"
 	"math"
+	"sort"
+	"sync"
 	"time"
 )
 
-const (
-	Width  = 800
-	Height = 600
+var (
+	Width  = int64(800)
+	Height = int64(600)
 )
 
 type ThreeDWidget struct {
@@ -28,9 +30,10 @@ type ThreeDWidget struct {
 func NewThreeDWidget() *ThreeDWidget {
 	w := &ThreeDWidget{}
 	w.ExtendBaseWidget(w)
-	w.camera = NewCamera(Point3D{X: 0, Y: 0, Z: 250}, Point3D{X: 0}, 1, 100)
+	w.camera = NewCamera(Point3D{X: 0, Y: 500, Z: 200}, Point3D{X: 0}, 30, 10)
 	w.image = canvas.NewImageFromImage(w.render())
 	go w.animate()
+	w.camera.PointAt(Point3D{X: 0, Y: 0, Z: 100})
 	return w
 }
 
@@ -50,28 +53,78 @@ func (w *ThreeDWidget) CreateRenderer() fyne.WidgetRenderer {
 }
 
 func (w *ThreeDWidget) render() image.Image {
-	img := image.NewRGBA(image.Rect(0, 0, Width, Height))
+	img := image.NewRGBA(image.Rect(0, 0, int(Width), int(Height)))
 	draw.Draw(img, img.Bounds(), &image.Uniform{C: color.RGBA{A: 0}}, image.Point{}, draw.Src)
 
 	plane := NewPlane(1000, Point3D{X: 0, Y: 0, Z: 0}, Point3D{X: 0, Y: 0, Z: 0}, color.RGBA{G: 255, A: 255}, w)
-	cube := NewCube(100, Point3D{X: 0, Y: 0, Z: 100}, Point3D{X: w.angleX, Y: w.angleY, Z: w.angleZ}, color.RGBA{R: 255, A: 255}, w)
+	cube := NewCube(100, Point3D{X: 0, Y: 0, Z: 100}, Point3D{X: w.angleX, Y: w.angleY, Z: w.angleZ}, color.RGBA{B: 255, A: 255}, w)
 
-	plane.Draw(img)
-	cube.Draw(img)
+	objects := []ThreeDShape{plane, cube}
+
+	var edges []struct {
+		edge     [2]Point3D
+		color    color.Color
+		distance float64
+	}
+
+	getEdgesWaitGroup := sync.WaitGroup{}
+	getEdgesWaitGroup.Add(len(objects))
+	for _, object := range objects {
+		go func(object ThreeDShape) {
+			defer getEdgesWaitGroup.Done()
+			objectEdges := object.GetEdges()
+			for _, edge := range objectEdges {
+				edges = append(edges, struct {
+					edge     [2]Point3D
+					color    color.Color
+					distance float64
+				}{edge: edge.edge, color: edge.color, distance: w.edgeDistance(edge.edge)})
+			}
+		}(object)
+	}
+	getEdgesWaitGroup.Wait()
+
+	sort.Slice(edges, func(i, j int) bool {
+		return edges[i].distance > edges[j].distance
+	})
+
+	for _, edge := range edges {
+		start := w.camera.Project(edge.edge[0])
+		end := w.camera.Project(edge.edge[1])
+
+		startInBounds := start.X >= 0 && start.X < Width && start.Y >= 0 && start.Y < Height
+		endInBounds := end.X >= 0 && end.X < Width && end.Y >= 0 && end.Y < Height
+
+		if !startInBounds && !endInBounds {
+			continue
+		}
+		if !startInBounds {
+			drawLine(img, end, start, edge.color)
+		} else {
+			drawLine(img, start, end, edge.color)
+		}
+	}
 
 	return img
 }
 
+func (w *ThreeDWidget) edgeDistance(edge [2]Point3D) float64 {
+	cameraPos := w.camera.Position
+	dist1 := math.Sqrt(math.Pow(edge[0].X-cameraPos.X, 2) + math.Pow(edge[0].Y-cameraPos.Y, 2) + math.Pow(edge[0].Z-cameraPos.Z, 2))
+	dist2 := math.Sqrt(math.Pow(edge[1].X-cameraPos.X, 2) + math.Pow(edge[1].Y-cameraPos.Y, 2) + math.Pow(edge[1].Z-cameraPos.Z, 2))
+	return (dist1 + dist2) / 2
+}
+
 func (w *ThreeDWidget) Dragged(event *fyne.DragEvent) {
-	w.RotateCameraAroundPoint(Point3D{X: 0, Y: 0, Z: 0}, float64(event.Dragged.DY/10), float64(event.Dragged.DX/10), 0)
-	w.PointCameraAt(Point3D{X: 0, Y: 0, Z: 0})
+	w.RotateCameraAroundPoint(Point3D{X: 0, Y: 0, Z: 100}, float64(event.Dragged.DY/10), 0, float64(event.Dragged.DX/10))
+	w.PointCameraAt(Point3D{X: 0, Y: 0, Z: 100})
 }
 
 func (w *ThreeDWidget) DragEnd() {}
 
 func (w *ThreeDWidget) Scrolled(event *fyne.ScrollEvent) {
 	w.camera.MoveForward(float64(event.Scrolled.DY) / 3)
-	w.PointCameraAt(Point3D{X: 0, Y: 0, Z: 0})
+	w.PointCameraAt(Point3D{X: 0, Y: 0, Z: 100})
 	w.image.Image = w.render()
 	canvas.Refresh(w.image)
 }
@@ -82,6 +135,8 @@ type threeDRenderer struct {
 
 func (r *threeDRenderer) Layout(size fyne.Size) {
 	r.image.Resize(size)
+	Width = int64(size.Width)
+	Height = int64(size.Height)
 }
 
 func (r *threeDRenderer) MinSize() fyne.Size {
@@ -118,10 +173,10 @@ func (w *ThreeDWidget) RotateCamera(dPitch, dYaw, dRoll float64) {
 	canvas.Refresh(w.image)
 }
 
-func (w *ThreeDWidget) RotateCameraAroundPoint(point Point3D, dPitch, dYaw, dRoll float64) {
-	w.camera.Position = rotateX(w.camera.Position, point, dPitch)
-	w.camera.Position = rotateY(w.camera.Position, point, dYaw)
-	w.camera.Position = rotateZ(w.camera.Position, point, dRoll)
+func (w *ThreeDWidget) RotateCameraAroundPoint(point Point3D, x, y, z float64) {
+	w.camera.Position = rotateX(w.camera.Position, point, x)
+	w.camera.Position = rotateY(w.camera.Position, point, y)
+	w.camera.Position = rotateZ(w.camera.Position, point, z)
 	w.image.Image = w.render()
 	canvas.Refresh(w.image)
 }
@@ -132,27 +187,25 @@ func (w *ThreeDWidget) PointCameraAt(target Point3D) {
 	canvas.Refresh(w.image)
 }
 
-func drawLine(img *image.RGBA, point1, point2 Point2D, col color.Color, retry bool) {
-	x0 := int(point1.X)
-	y0 := int(point1.Y)
-	x1 := int(point2.X)
-	y1 := int(point2.Y)
+func drawLine(img *image.RGBA, point1, point2 Point2D, lineColor color.Color) {
+	x0 := point1.X
+	y0 := point1.Y
+	x1 := point2.X
+	y1 := point2.Y
 	dx := abs(x1 - x0)
 	dy := abs(y1 - y0)
-	sx := -1
+	sx := int64(-1)
 	if x0 < x1 {
-		sx = 1
+		sx = int64(1)
 	}
-	sy := -1
+	sy := int64(-1)
 	if y0 < y1 {
-		sy = 1
+		sy = int64(1)
 	}
 	err := dx - dy
 
-	maxIterations := dx + dy + 1
-
-	for i := 0; i < maxIterations; i++ {
-		img.Set(x0, y0, col)
+	for {
+		img.Set(int(x0), int(y0), lineColor)
 		if x0 == x1 && y0 == y1 {
 			break
 		}
@@ -165,19 +218,10 @@ func drawLine(img *image.RGBA, point1, point2 Point2D, col color.Color, retry bo
 			err += dx
 			y0 += sy
 		}
-		if (sx > 0 && x0 > x1) || (sx < 0 && x0 < x1) || (sy > 0 && y0 > y1) || (sy < 0 && y0 < y1) {
-			break
-		}
-		if x0 < 0 || x0 >= img.Bounds().Dx() || y0 < 0 || y0 >= img.Bounds().Dy() {
-			if !retry {
-				drawLine(img, point2, point1, col, true)
-			}
-			break
-		}
 	}
 }
 
-func abs(x int) int {
+func abs(x int64) int64 {
 	if x < 0 {
 		return -x
 	}
@@ -207,13 +251,13 @@ func (camera *Camera) Project(point Point3D) Point2D {
 	translatedZ := point.Z - camera.Position.Z
 
 	if translatedZ == 0 {
-		translatedZ = 0.1
+		translatedZ = 0.000001
 	}
 
-	x2D := (translatedX*camera.FocalLength/translatedZ)*camera.Scale + Width/2
-	y2D := (translatedY*camera.FocalLength/translatedZ)*camera.Scale + Height/2
+	x2D := (translatedX*camera.FocalLength/translatedZ)*camera.Scale + float64(Width)/2
+	y2D := (translatedY*camera.FocalLength/translatedZ)*camera.Scale + float64(Height)/2
 
-	return Point2D{x2D, y2D}
+	return Point2D{int64(x2D), int64(y2D)}
 }
 
 func (camera *Camera) PointAt(target Point3D) {
@@ -249,12 +293,12 @@ type Point3D struct {
 }
 
 type Point2D struct {
-	X, Y float64
+	X, Y int64
 }
 
 type ThreeDShape struct {
-	Vertices [8]Point3D
-	Edges    [12][2]int
+	Vertices []Point3D
+	Edges    [][2]int
 	Rotation Point3D
 	Position Point3D
 	color    color.Color
@@ -264,13 +308,13 @@ type ThreeDShape struct {
 func NewCube(size float64, position Point3D, rotation Point3D, color color.Color, w *ThreeDWidget) ThreeDShape {
 	half := size / 2
 	return ThreeDShape{
-		Vertices: [8]Point3D{
+		Vertices: []Point3D{
 			{-half, -half, -half}, {half, -half, -half},
 			{half, half, -half}, {-half, half, -half},
 			{-half, -half, half}, {half, -half, half},
 			{half, half, half}, {-half, half, half},
 		},
-		Edges: [12][2]int{
+		Edges: [][2]int{
 			{0, 1}, {1, 2}, {2, 3}, {3, 0},
 			{4, 5}, {5, 6}, {6, 7}, {7, 4},
 			{0, 4}, {1, 5}, {2, 6}, {3, 7},
@@ -283,19 +327,32 @@ func NewCube(size float64, position Point3D, rotation Point3D, color color.Color
 }
 
 func NewPlane(size float64, position Point3D, rotation Point3D, color color.Color, w *ThreeDWidget) ThreeDShape {
+	gridSize := int(size / 10)
 	half := size / 2
+	numVertices := (gridSize + 1) * (gridSize + 1)
+	vertices := make([]Point3D, numVertices)
+	edges := make([][2]int, 0)
+
+	index := 0
+	for i := 0; i <= gridSize; i++ {
+		for j := 0; j <= gridSize; j++ {
+			x := -half + float64(i)*10
+			y := -half + float64(j)*10
+			vertices[index] = Point3D{X: x, Y: y, Z: 0}
+			index++
+		}
+	}
+
+	for i := 0; i <= gridSize; i++ {
+		for j := 0; j < gridSize; j++ {
+			edges = append(edges, [2]int{i*(gridSize+1) + j, i*(gridSize+1) + j + 1})
+			edges = append(edges, [2]int{j*(gridSize+1) + i, (j+1)*(gridSize+1) + i})
+		}
+	}
+
 	return ThreeDShape{
-		Vertices: [8]Point3D{
-			{-half, -half, 0}, {half, -half, 0},
-			{half, half, 0}, {-half, half, 0},
-			{-half, -half, 0}, {half, -half, 0},
-			{half, half, 0}, {-half, half, 0},
-		},
-		Edges: [12][2]int{
-			{0, 1}, {1, 2}, {2, 3}, {3, 0},
-			{4, 5}, {5, 6}, {6, 7}, {7, 4},
-			{0, 4}, {1, 5}, {2, 6}, {3, 7},
-		},
+		Vertices: vertices,
+		Edges:    edges,
 		Position: position,
 		Rotation: rotation,
 		color:    color,
@@ -303,8 +360,16 @@ func NewPlane(size float64, position Point3D, rotation Point3D, color color.Colo
 	}
 }
 
-func (shape *ThreeDShape) Draw(img *image.RGBA) {
-	for _, edge := range shape.Edges {
+func (shape *ThreeDShape) GetEdges() []struct {
+	edge  [2]Point3D
+	color color.Color
+} {
+	edges := make([]struct {
+		edge  [2]Point3D
+		color color.Color
+	}, len(shape.Edges))
+
+	for i, edge := range shape.Edges {
 		start := shape.Vertices[edge[0]]
 		end := shape.Vertices[edge[1]]
 
@@ -324,11 +389,13 @@ func (shape *ThreeDShape) Draw(img *image.RGBA) {
 		end.Y += shape.Position.Y
 		end.Z += shape.Position.Z
 
-		point1 := shape.w.camera.Project(start)
-		point2 := shape.w.camera.Project(end)
-
-		drawLine(img, point1, point2, shape.color, false)
+		edges[i] = struct {
+			edge  [2]Point3D
+			color color.Color
+		}{edge: [2]Point3D{start, end}, color: shape.color}
 	}
+
+	return edges
 }
 
 func degreesToRadians(degrees float64) float64 {
